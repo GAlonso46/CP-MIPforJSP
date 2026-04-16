c2e15a60a10069380b99a790e5622c655422858e
RC46 <gaboac46@gmail.com>
2026-04-16 12:33:44 -0500

    refactor: adjust JSON loader to new JSON instances representation

--- utils/load_json.py
+++ utils/load_json.py
@@ -18,10 +18,12 @@ def load_extended_jssp_instance(file_path):
     def to_t(s): 
         parts = s.split('_')
         return (int(parts[0]), int(parts[1]))
+    
+    workers = data.get("workers") or []
 
     res = {
         "machines": data["machines"],
-        "workers": data["workers"],
+        "workers": workers,
         "tasks": [to_t(t) for t in data["tasks"]],
         "job_tasks": {int(j): [to_t(t) for t in ts] for j, ts in data["job_tasks"].items()},
         "P": [(to_t(p[0]), to_t(p[1])) for p in data["P"]],
@@ -36,14 +38,15 @@ def load_extended_jssp_instance(file_path):
     }
 
     # Reconstruct L: "job_op|job_op" -> ((job, op), (job, op))
-    for k, v in data["L"].items():
+    for k, v in data.get("L", {}).items():
         t1_s, t2_s = k.split('|')
         res["L"][(to_t(t1_s), to_t(t2_s))] = v
 
     # Reconstruct p: "job_op|m|w" -> ((job, op), m, w)
-    for k, v in data["p"].items():
+    for k, v in data.get("p", {}).items():
         t_s, m_s, w_s = k.split('|')
-        res["p"][(to_t(t_s), int(m_s), int(w_s))] = v
+        actual_w = None if w_s == 'null' else int(w_s)
+        res["p"][(to_t(t_s), int(m_s), actual_w)] = v
 
     # Reconstruct s: "job_i_op_i|job_k_op_k|m" -> (task_i, task_k, m)
     for k, v in data.get("s", {}).items():
